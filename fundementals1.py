824eb1b881af040d2b38587757d3f74d5014e94d
rahulcbaraik <39028855+rahulcbaraik@users.noreply.github.com>
2025-10-08 19:44:57 +0530

    practicing problems

---
+++ fundementals1.py
@@ -0,0 +1,6 @@
+score = {"Alice": 42, "Charlie": 48}
+for_alice = score["Alice"]
+floatnum = float(for_alice)
+percentage = (floatnum / 50) * 100
+print(f"Alice's Final Score:{for_alice}, {percentage:.1f}%")
+
