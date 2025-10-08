aa48b7449b1bbd418bdec8757cd1de05f6e83a63
rahulcbaraik <39028855+rahulcbaraik@users.noreply.github.com>
2025-10-08 22:15:06 +0530

    Just another program

---
+++ fundamentals2.py
@@ -0,0 +1,8 @@
+phrase_list = ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g', 'i', 's', 'f', 'u', 'n']
+exclude = "o"
+new_list = "".join(phrase_list)
+modified_list = new_list.replace("o","")
+modified_list1 = modified_list.upper()
+print(modified_list1)
+
+
